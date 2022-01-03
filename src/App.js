292ba09217697251f8b4dca9c31c1d62eb9bd774
mntak <32028272+mntak@users.noreply.github.com>
2022-01-03 04:29:45 -1000

    styled footer

--- src/App.js
+++ src/App.js
@@ -8,11 +8,38 @@ export default function App() {
       <div className="container">
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1> dictionary</h1>
         </header>
+
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
-        <footer className="App-footer">Coded by Mihae Tak</footer>
+        <footer className="App-footer">
+          Coded by {""}
+          <a
+            href="https://friendly-mayer-5513d1.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mihae Tak
+          </a>
+          , open-sourced on {""}
+          <a
+            href="https://github.com/mntak/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          , hosted on
+          <a
+            href="https://cocky-darwin-28723b.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
