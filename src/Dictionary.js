f3d9144cca043f5e2b97758028847290f1d278db
mntak <32028272+mntak@users.noreply.github.com>
2022-01-02 21:54:11 -1000

    Added photo api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,34 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState (null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse (response) {
+      console.log(response);
+      setPhotos(response.data.photos);
   }
 
   function search() {
     //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey = "563492ad6f9170000100000115f0db8985c0497896ebd9320ffc634f"
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`; 
+    let headers = { Authorization: `Bearer ${pexelsApiKey}`};
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+     
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -46,6 +59,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos}/>
       </div>
     );
   } else {
