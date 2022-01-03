764be29f8bb36463218edeefdcf5733ce7e6c38b
mntak <32028272+mntak@users.noreply.github.com>
2022-01-02 18:19:34 -1000

    Rendered the world meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,32 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState(null);
+  let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
-    function handleResponse(response) {
-        console.log(response.data[0]);
-    }
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
-    function search(event) {
-        event.preventDefault();
-        
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-       
-        axios.get(apiUrl).then(handleResponse);
+  function search(event) {
+    event.preventDefault();
 
-    }
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
-    function handleKeywordChange (event) {
-        setKeyword(event.target.value);
-    }
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} autoFocus={true} />
       </form>
+
+      <Results results={results} />
     </div>
   );
 }
