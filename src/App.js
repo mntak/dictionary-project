3e0c6c02346304f5260ab96a882bd73de9d4fccf
mntak <32028272+mntak@users.noreply.github.com>
2022-01-02 16:59:03 -1000

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.png'
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Mihae Tak
+        </footer>
+      </div>
     </div>
   );
 }
-
-
